6a7ec75fcd88ea9a49c1ed2630ac1d51a9c0cf83
root <root@LAPTOP-2SCA53CF.localdomain>
2021-10-02 17:36:18 +0300

    Second commit #0002

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import praw
 
 reddit = praw.Reddit(
-
+    client_id="MFTZx6wu8I6lG-Fbi7j2lw",
+    client_secret="tGMzNhKV23yRH7scYQB67XDLfUXVLg",
+    user_agent="https://github.com/erdemozturkfun/rapibot",
+    username="Asrielthemonster3",
+    password="erdem2009.",
 )
+
+subreddit = reddit.subreddit("memes")
+
+
+print(subreddit.title)
+# Output: False
